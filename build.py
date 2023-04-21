cc1da6c4fa964f92b8f79cb3407835afcf44f05d
gferraro <g.ferraro22@gmail.com>
2023-04-21 15:44:15 +1200

    fix indent

--- build.py
+++ build.py
@@ -306,20 +306,19 @@ def trim_noise(dataset):
                 elif t_s is not None:
                     # Done
                     break
-        offset += s[1] - s[0]
-        if t.start is None:
-            logging.warn("Rec %s track %s has no signal data", r.id, t.id)
-            tracks_del.append(t)
-        # r.tracks.remove()
-        # print("track ", t.start, t.end, " now has", t_s, t_e, t.human_tags)
-        t.start = t_s
-        t.end = t_e
-    for t in tracks_del:
-        r.tracks.remove(t)
-    r.recalc_tags()
-    r.samples = []
-    r.load_samples(dataset.config.segment_length, dataset.config.segment_stride)
-    dataset.samples.extend(r.samples)
+            if t_s is None:
+                logging.warn("Rec %s track %s has no signal data", r.id, t.id)
+                tracks_del.append(t)
+            # r.tracks.remove()
+            # print("track ", t.start, t.end, " now has", t_s, t_e, t.human_tags)
+            t.start = t_s
+            t.end = t_e
+        for t in tracks_del:
+            r.tracks.remove(t)
+        r.recalc_tags()
+        r.samples = []
+        r.load_samples(dataset.config.segment_length, dataset.config.segment_stride)
+        dataset.samples.extend(r.samples)
 
 
 def main():
