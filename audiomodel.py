2e58cc41e8fa32a4f27ed1420e3c7925d184fd98
gferraro <g.ferraro22@gmail.com>
2023-05-02 15:10:21 +1200

    adjust

--- audiomodel.py
+++ audiomodel.py
@@ -460,6 +460,7 @@ class AudioModel:
                 tf.keras.metrics.AUC(),
                 tf.keras.metrics.Recall(),
                 tf.keras.metrics.Precision(),
+                macro_f1,
             ],
         )
 
@@ -759,7 +760,7 @@ def loss(multi_label=False, smoothing=0):
         # return tf.keras.losses.BinaryFocalCrossentropy(
         #     gamma=2.0, from_logits=False, apply_class_balancing=True
         # )
-        return macro_soft_f1
+        return macro_f1
         logging.info("Using binary")
         softmax = tf.keras.losses.BinaryCrossentropy(
             label_smoothing=smoothing,
@@ -1147,6 +1148,26 @@ def macro_soft_f1(y, y_hat):
     macro_cost = tf.reduce_mean(cost)  # average on all labels
 
     return macro_cost
+
+
+def macro_f1(y, y_hat, thresh=0.5):
+    """Compute the macro F1-score on a batch of observations (average F1 across labels)
+
+    Args:
+        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        thresh: probability value above which we predict positive
+
+    Returns:
+        macro_f1 (scalar Tensor): value of macro F1 for the batch
+    """
+    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
+    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
+    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
+    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
+    f1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
+    macro_f1 = tf.reduce_mean(f1)
+    return macro_f1
 
 
 def macro_double_soft_f1(y, y_hat):
