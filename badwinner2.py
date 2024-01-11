2cde752ac26e2433f79322e052ad89b387bf47e7
gferraro <g.ferraro22@gmail.com>
2024-01-11 14:03:37 +1300

    default

--- badwinner2.py
+++ badwinner2.py
@@ -313,7 +313,7 @@ def main():
     init_logging()
     args = parse_args()
     model = build_model(
-        (160, 513, 1), None, 21, multi_label=True, lme=False, big_condense=False
+        (160, 513, 1), None, 21, multi_label=True, lme=False, big_condense=True
     )
     model.summary()
     model.compile(
